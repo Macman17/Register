75f950c34a3665277e142a6d263b677e9794687c
Macman17 <60798387+Macman17@users.noreply.github.com>
2022-03-17 18:07:46 -0700

    beginning
    Add all files

---
+++ Java/register.js
@@ -0,0 +1,10 @@
+let userList = []
+function User(firstName, lastName, email, password, cardNumber) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.password = password;
+    this.cardNumber = password;
+}
+
+let shay= new User('shay','cerny','')
