44b27d9f729ee7efcb73724b7b6232f96202d5d0
Macman17 <60798387+Macman17@users.noreply.github.com>
2022-03-20 08:50:14 -0700

    Uploaded main files
    Created main HTML, CSS, and JS

--- Java/register.js
+++ Java/register.js
@@ -1,10 +1,49 @@
 let userList = []
-function User(firstName, lastName, email, password, cardNumber) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.email = email;
-    this.password = password;
-    this.cardNumber = password;
+
+class User{
+    constructor (firstName, lastName, email, age, address, password, cardNumber,phone, color){ 
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age= age;
+        this.address= address;
+        this.password = password;
+        this.cardNumber = cardNumber;
+        this.contactPhone= phone; //#attention###
+        this.color= color;
+    }
 }
 
-let shay= new User('shay','cerny','')
+function register(){
+    let inputFirstName= $('#txtFirstName').val();
+    let inputLastName= $('#txtLastName').val();
+    let inputEmail= $('#txtEmail').val();
+    let inputPassword= $('#txtPassword').val();
+    let inputage= $("#txtAge").val();
+    let inputCardNumber= $('#txtCardNumber').val();
+    let inputAddress= $("#txtAddress").val();
+    let inputPhone= $("#txtPhone").val();
+    let inputColor= $("#txtColor").val();
+
+    console.log(inputFirstName,inputLastName,inputEmail,inputPassword,inputage,inputCardNumber,inputAddress,inputPhone,inputColor);
+    //create theUser
+    let theUser = new User(inputFirstName,inputLastName,inputEmail,inputPassword,inputage,inputCardNumber,inputAddress,inputPhone,inputColor);
+    //validate the user *** Extra Home work
+    if (theUser.email=="") {
+        console.log("User not added");
+    
+    }
+    else{
+        console.log("User Added");
+    }
+    console.log(theUser);
+    //clear the inputs
+    $("input").val("");
+}
+function init() {
+   console.log("Registration");
+    //hook events
+
+}
+window.onload=init;
+ 
